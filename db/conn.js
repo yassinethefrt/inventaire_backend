9705ea0b4de975b1b41acf22fa9457954af62eac
yassinethefrt <yassinefartassi01@gmail.com>
2023-12-21 21:34:01 +0100

    update infos

--- db/conn.js
+++ db/conn.js
@@ -3,18 +3,22 @@ const sql = require("mssql");
 const dbSettings = {
   user: "admin",
   password: "Meekmill..1",
-  server: "68.64.164.91",
+  server: "163.123.183.91",
   database: "inventaire_db",
-  port: 15873, // Update this line with your new port number
+  port: 18679,
   options: {
     encrypt: false,
-    // You can add other options as needed
   },
 };
 
+// Create a connection pool
+const pool = new sql.ConnectionPool(dbSettings);
+const poolConnect = pool.connect();
+
 exports.getConnection = async () => {
   try {
-    const pool = await sql.connect(dbSettings);
+    // Wait for the pool to connect before returning it
+    await poolConnect;
     return pool;
   } catch (error) {
     console.error(error);
@@ -28,18 +32,16 @@ exports.getSql = () => {
 // Query user by email
 exports.getUserByEmail = async (email) => {
   try {
-    // Connect to SQL Server
-    await sql.connect(dbSettings);
+    // Wait for the pool to connect before executing queries
+    await poolConnect;
 
     // Query user by email
-    const result = await sql.query`SELECT * FROM users WHERE email = ${email}`;
+    const result = await pool.request()
+      .query`SELECT * FROM users WHERE email = ${email}`;
     const user = result.recordset[0];
 
     return user;
   } catch (error) {
     throw error;
-  } finally {
-    // Close SQL Server connection
-    sql.close();
   }
 };
